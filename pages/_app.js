5390a7107dd7be60b10551c1ef02761fe8961545
RareBay <150396857+RareBay@users.noreply.github.com>
2023-12-31 04:19:39 +0000

    ufgh

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import "./style.css";
 import { CoreBlockchain } from "@thirdweb-dev/chains";
-import { OKXWallet, ThirdwebProvider, metamaskWallet, useContract } from "@thirdweb-dev/react";
+import { OKXWallet, ThirdwebProvider, metamaskWallet, okxWallet, useContract } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  coinbaseWallet,
+  walletConnect,
+  localWallet,
+  embeddedWallet,
+} from "@thirdweb-dev/react";
 
 import React from "react";
 export default function MyApp({ Component: Component, pageProps: pageProps }) {
   return (
     <ThirdwebProvider
+    supportedWallets={[
+      metamaskWallet({ recommended: true }),
+      walletConnect({recommended: true}),
+      okxWallet,
+      localWallet()
+    ]}
       activeChain={CoreBlockchain}
       clientId="cc3b7731ad9935953abe4a625abea530" // You can get a client id from dashboard settings
     >
