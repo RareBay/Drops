c588c8d777de54825b05dee1fa89f5891aaf341b
RareBay <150396857+RareBay@users.noreply.github.com>
2023-12-31 04:24:59 +0000

    hkmg

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { CoreBlockchain } from "@thirdweb-dev/chains";
-import { OKXWallet, ThirdwebProvider, metamaskWallet, okxWallet, useContract } from "@thirdweb-dev/react";
+import { CoinbaseWallet, OKXWallet, ThirdwebProvider, metamaskWallet, okxWallet, useContract } from "@thirdweb-dev/react";
 import {
   ConnectWallet,
   coinbaseWallet,
@@ -16,8 +16,6 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
     supportedWallets={[
       metamaskWallet({ recommended: true }),
       walletConnect({recommended: true}),
-      okxWallet,
-      localWallet()
     ]}
       activeChain={CoreBlockchain}
       clientId="cc3b7731ad9935953abe4a625abea530" // You can get a client id from dashboard settings
